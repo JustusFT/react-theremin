097d2810260e2a19b79835438e16e9a86112cbce
JustusFT <JustusFT@gmail.com>
2018-07-28 15:16:52 +0800

    Split oscillator logic from App component

--- src/App.js
+++ src/App.js
@@ -1,63 +1,30 @@
 import React, { Component } from 'react';
 import Options from './Options';
 import PlayField from './PlayField';
-
-import { calculatePitch } from './util';
-
-const mockOptions = {
-  volumeArea: 50,
-  range: [80, 1440]
-  //key: 'C',
-  //scale: 'major'
-};
-
-// create web audio api context
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
-// create Oscillator node
-var oscillator = audioCtx.createOscillator();
-oscillator.type = 'sine';
-
-var gainNode = audioCtx.createGain();
-oscillator.connect(gainNode);
-oscillator.start();
+import ThereminOscillator from './ThereminOscillator';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.playSound = this.playSound.bind(this);
-    this.stopSound = this.stopSound.bind(this);
+    this.thereminOscillator = new ThereminOscillator();
     this.handleMouseMove = this.handleMouseMove.bind(this);
-    this.setPitch = this.setPitch.bind(this);
   }
 
   handleMouseMove(e) {
-    this.setPitch(e.pageX);
-  }
-
-  playSound() {
-    gainNode.connect(audioCtx.destination);
-  }
-
-  stopSound() {
-    gainNode.disconnect(audioCtx.destination);
-  }
-
-  // set the oscillator's pitch based on the x position given
-  setPitch(x) {
-    oscillator.frequency.value = calculatePitch(x, mockOptions.range);
+    this.thereminOscillator.setPitch(e.pageX);
   }
 
   render() {
+    const { options } = this.thereminOscillator;
     return (
       <div>
         <Options />
         <PlayField
-          range={mockOptions.range}
-          volumeArea={mockOptions.volumeArea}
-          onMouseEnter={this.playSound}
-          onMouseLeave={this.stopSound}
+          range={options.range}
+          volumeArea={options.volumeArea}
+          onMouseEnter={this.thereminOscillator.playSound}
+          onMouseLeave={this.thereminOscillator.stopSound}
           onMouseMove={this.handleMouseMove}
         />
       </div>
---
+++ src/ThereminOscillator.js
@@ -0,0 +1,45 @@
+import { calculatePitch } from './util';
+
+export default class ThereminOscillator {
+  constructor() {
+    // create web audio api context
+    this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+    // oscillator node
+    this.oscillator = this.audioCtx.createOscillator();
+    this.oscillator.type = 'sine';
+
+    // gain node
+    this.gainNode = this.audioCtx.createGain();
+    this.oscillator.connect(this.gainNode);
+
+    // activate
+    this.oscillator.start();
+
+    // bindings
+    this.playSound = this.playSound.bind(this);
+    this.stopSound = this.stopSound.bind(this);
+    this.setPitch = this.setPitch.bind(this);
+
+    // options object
+    this.options = {
+      volumeArea: 50,
+      range: [80, 1440]
+      //key: 'C',
+      //scale: 'major'
+    };
+  }
+
+  playSound() {
+    this.gainNode.connect(this.audioCtx.destination);
+  }
+
+  stopSound() {
+    this.gainNode.disconnect(this.audioCtx.destination);
+  }
+
+  // set the oscillator's pitch based on the x position given
+  setPitch(x) {
+    this.oscillator.frequency.value = calculatePitch(x, this.options.range);
+  }
+}
