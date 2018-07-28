1bbef5b0875793c162326853aae50ad88f8bffce
JustusFT <JustusFT@gmail.com>
2018-07-28 10:18:04 +0800

    Play monotone audio, but only when the mouse enters the PlayField

--- src/App.js
+++ src/App.js
@@ -9,12 +9,49 @@ const mockOptions = {
   //scale: 'major'
 };
 
+// create web audio api context
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+// create Oscillator node
+var oscillator = audioCtx.createOscillator();
+oscillator.type = 'sine';
+oscillator.frequency.setValueAtTime(440, audioCtx.currentTime); // value in hertz
+
+var gainNode = audioCtx.createGain();
+oscillator.connect(gainNode);
+oscillator.start();
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.playSound = this.playSound.bind(this);
+    this.stopSound = this.stopSound.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+  }
+
+  playSound() {
+    gainNode.connect(audioCtx.destination);
+  }
+
+  stopSound() {
+    gainNode.disconnect(audioCtx.destination);
+  }
+
+  handleMouseMove(e) {
+    console.log(e);
+  }
+
   render() {
     return (
       <div>
         <Options />
-        <PlayField options={mockOptions} />
+        <PlayField
+          options={mockOptions}
+          onMouseEnter={this.playSound}
+          onMouseLeave={this.stopSound}
+          onMouseMove={this.handleMouseMove}
+        />
       </div>
     );
   }
--- src/PlayField.js
+++ src/PlayField.js
@@ -2,9 +2,14 @@ import React from 'react';
 import VolumeField from './VolumeField';
 import './PlayField.css';
 
-function PlayField({ options }) {
+function PlayField({ options, ...props }) {
   return (
-    <div className="PlayField">
+    <div
+      className="PlayField"
+      onMouseEnter={props.onMouseEnter}
+      onMouseLeave={props.onMouseLeave}
+      onMouseMove={props.onMouseMove}
+    >
       <VolumeField height={options.volumeArea} />
     </div>
   );
