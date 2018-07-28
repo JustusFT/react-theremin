825b2c80a61c24503ad3f374e3b7e30e11641a8a
JustusFT <JustusFT@gmail.com>
2018-07-28 08:57:45 +0800

    Add normalize.css

--- src/Options.js
+++ src/Options.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import Slider, { Range } from 'rc-slider';
-// We can just import Slider or Range to reduce bundle size
-// import Slider from 'rc-slider/lib/Slider';
-// import Range from 'rc-slider/lib/Range';
 import 'rc-slider/assets/index.css';
 
 function Options() {
